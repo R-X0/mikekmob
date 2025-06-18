3a4931e31c334abd8b5e0ff35c4e3f82e133af42
root <root@DESKTOP-6232HHC.>
2025-06-18 09:22:11 +0000

    change

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+def main():
+    name = input("What's your name? ")
+    if name.strip():
+        print(f"Hello, {name}!")
+    else:
+        print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
